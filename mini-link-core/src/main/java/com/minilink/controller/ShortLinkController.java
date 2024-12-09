cfcb8f24f4b8e245f25b975530cb80d6637a2c7a
徐志斌 <binx19980707@gmail.com>
2024-12-09 17:11:19 +0800

    feat:整合ShardingJDBC,实现分库分表相关功能

--- mini-link-core/src/main/java/com/minilink/controller/ShortLinkController.java
+++ mini-link-core/src/main/java/com/minilink/controller/ShortLinkController.java
@@ -1,6 +1,5 @@
 package com.minilink.controller;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -12,19 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/link")
 public class ShortLinkController {
-    @ApiOperation("生成短链接")
     @PostMapping("/createShortLink/{longLink}")
     public void createShort(@PathVariable String longLink) {
 
     }
 
-    @ApiOperation("查询长链接")
     @GetMapping("/getLongLink/{shortLink}")
     public void getLongLink(@PathVariable String shortLink) {
 
     }
 
-    @ApiOperation("链接跳转（短链接 ——> 原链接）")
     @GetMapping("/route/{shortLink}")
     public void route(@PathVariable String shortLink) {
 
