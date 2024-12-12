13420020e02f41837f4badfba468e829fca3081b
徐志斌 <binx19980707@gmail.com>
2024-12-12 14:20:11 +0800

    feat：基于Guava 实现 MurmurHash算法生成短链接

--- mini-link-common/src/main/java/com/minilink/util/ShortLinkUtil.java
+++ mini-link-common/src/main/java/com/minilink/util/ShortLinkUtil.java
@@ -1,38 +1,32 @@
 package com.minilink.util;
 
-import cn.hutool.core.lang.hash.MurmurHash;
+import com.google.common.hash.Hashing;
 
 /**
  * @Author: 徐志斌
  * @CreateTime: 2024-12-12  13:12
- * @Description: 短链接工具类
+ * @Description: 短链接工具类：Murmurhash算法 + Base62编码
  * @Version: 1.0
  */
 public class ShortLinkUtil {
-    private static final char[] CHARS = new char[]{
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    };
-    private static final int SIZE = CHARS.length;
-
-    private static String convertDecToBase62(long num) {
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            int i = (int) (num % SIZE);
-            sb.append(CHARS[i]);
-            num /= SIZE;
-        }
-        return sb.reverse().toString();
-    }
-
-    private static String hash32ToBase62(String str) {
-        int i = MurmurHash.hash32(str);
-        long num = i < 0 ? Integer.MAX_VALUE - (long) i : i;
-        return convertDecToBase62(num);
-    }
+    private static final String CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public static String generate(String link) {
-        return hash32ToBase62(link);
+        long murmurHash32 = murmurHash32(link);
+        return encodeToBase62(murmurHash32);
+    }
+
+    private static long murmurHash32(String str) {
+        return Hashing.murmur3_32_fixed().hashUnencodedChars(str).padToLong();
+    }
+
+    private static String encodeToBase62(long num) {
+        StringBuffer sb = new StringBuffer();
+        do {
+            int i = (int) (num % CHARS.length());
+            sb.append(CHARS.charAt(i));
+            num = num / CHARS.length();
+        } while (num > 0);
+        return sb.reverse().toString();
     }
 }
